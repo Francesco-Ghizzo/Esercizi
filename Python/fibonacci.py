1f2e123b7bc06e2b2ad2409ba8336f53198b9f81
Francesco-Ghizzo <61307774+Francesco-Ghizzo@users.noreply.github.com>
2022-01-03 23:04:44 +0100

    Update fibonacci.py
    mancava un 'else' nell'opzione 1

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -3,10 +3,11 @@
 def Fibonacci(num):
     if num < 3:
         return 1
-    a = b = 1
-    for i in range(2, num):
-        a, b = b, a+b
-    return b
+    else:
+        a = b = 1
+        for i in range(2, num):
+            a, b = b, a + b
+        return b
 
 
 # opzione 2
