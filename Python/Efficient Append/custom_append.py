a2ff1fa548807bbb5023e83f97580a8b75532b21
Francesco-Ghizzo <61307774+Francesco-Ghizzo@users.noreply.github.com>
2024-05-23 19:35:30 +0200

    Add files via upload

--- Python/Efficient Append/custom_append.py
+++ Python/Efficient Append/custom_append.py
@@ -1,27 +1,15 @@
 class PyList:
 
-    def __init__(self, size=0):
-        self.items = [None]*size
-        self.numItems = 0
-
-    def __iter__(self):
-        for item in self.items:
-            yield item
-
-    def __repr__(self) -> str:
-        return f"{(self).items}"
-
-
-    def inefficientAppend(self, item):
-        self.items = self.items + [item]
-
-
-    def efficientAppend(self, item):
-        if self.numItems == len(self.items):
-            newlst = [None]*nnew_allocated(self.numItems)
-        for i in  self.items:
-            newlst[i] = self.items[i]
-
+    def __init__(self, numItems=0):
+        allocated = 0
+        if numItems > 0:
+            while allocated < numItems:
+                allocated = new_allocated(allocated)
+        self.items = [None]*allocated
+        self.numItems = numItems
+        self.allocated = allocated
+        
+        
    # The growth pattern is:  0, 4, 8, 16, 25, 35, 46, 58, 72, 88, ...
 
     def new_allocated(oldsize):
@@ -30,3 +18,40 @@ class PyList:
         else:
             new_size = oldsize + 8 + (oldsize//8-1)
         return new_size
+
+    def __iter__(self):
+        for item in self.items:
+            yield item
+
+    # add this to implement printing your object:
+
+    def __repr__(self) -> str:
+        return f"{(self).items}"
+
+    # add this to implement sequence assignment in your custom class:
+
+    def __setitem__(self, key, value):
+        self.items[key] = value
+
+    def __getitem__(self, item):
+        return self.items[item]
+
+
+    # custom append methods
+
+    def inefficientAppend(self, item):
+        self.items = self.items + [item]
+
+
+    def efficientAppend(self, item):
+
+        if self.numItems == self.allocated:
+            newlst = [None]*new_allocated(self.numItems)
+            for i in  self.items:
+                newlst[i] = self.items[i]
+            
+            self.items = newlst
+
+        self.items[self.numItems] = item
+        self.allocated += 1
+        self.numItems += 1
