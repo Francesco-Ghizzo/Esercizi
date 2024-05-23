77fac8cb2765569a531ebcb74994a3b25a25e2a3
Francesco-Ghizzo <61307774+Francesco-Ghizzo@users.noreply.github.com>
2024-05-23 18:18:33 +0200

    Add files via upload

--- Python/Efficient Append/custom_append.py
+++ Python/Efficient Append/custom_append.py
@@ -10,6 +10,10 @@ class PyList:
     def __repr__(self) -> str:
         return f"{(self).items}"
 
+
+    def inefficientAppend(self, item):
+        self.items = self.items + [item]
+
     *The growth pattern is:  0, 4, 8, 16, 25, 35, 46, 58, 72, 88, ...
 
     def new_allocated(oldsize):
