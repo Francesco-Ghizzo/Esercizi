7979c52b63dfb17a037b2382b74b1d8b83d6ff88
Francesco-Ghizzo <61307774+Francesco-Ghizzo@users.noreply.github.com>
2025-06-25 02:48:23 +0200

    Update benchmark.py

--- Libri/Data Structures and Algorithms with Python/benchmark.py
+++ Libri/Data Structures and Algorithms with Python/benchmark.py
@@ -1,1 +1,10 @@
+import datetime
+import random
+import time
 
+def benchmark(func, *args):
+    starttime = datetime.datetime.now()
+        func(*args)
+    endtime = datetime.datetime.now()
+    deltaT = endtime - starttime
+        return deltaT
