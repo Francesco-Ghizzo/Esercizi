10379c4f6cddd724288ef02a26b278eb3d73a133
Francesco-Ghizzo <61307774+Francesco-Ghizzo@users.noreply.github.com>
2025-07-02 01:03:36 +0200

    Update custom_class.py

--- Python/Libri/Data Structures and Algorithms with Python/Custom List/custom_class.py
+++ Python/Libri/Data Structures and Algorithms with Python/Custom List/custom_class.py
@@ -2,10 +2,25 @@ class PyList:
 
     def __init__(self, size=0):
         self.items = [None]*size
+        
+    
+    # add this to iterate over all the values in the container:
 
     def __iter__(self):
         for item in self.items:
             yield item
+
+    # add this to implement sequence assignment in your custom class:
+
+    def __setitem__(self, key, value):
+        self.items[key] = value
+
+    # add this to implement retrieval of values in your custom class:
+
+    def __getitem__(self, item):
+        return self.items[item]
+
+    # add this to implement printing your object:
 
     def __repr__(self) -> str:
         return f"{(self).items}"
