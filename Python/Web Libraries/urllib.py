470b142be949fb0e1f3bceda6db9ff409d399f28
Francesco Ghizzo <francescoghizzo@MacBook-Pro-di-Francesco.local>
2025-07-04 00:09:16 +0200

    deleted:    Web Libraries/requests.py 	deleted:    Web Libraries/urllib.py

--- Python/Web Libraries/urllib.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-url = 'https://docs.python.org/3/library/concurrent.futures.html'
-
-def load_url(url, timeout):
-     with urllib.request.urlopen(url, timeout=timeout) as connection:
-         return connection.read()
-
-load_url(url, timeout=5)
